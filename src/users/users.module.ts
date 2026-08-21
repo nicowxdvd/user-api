6746310ed85b742f14cd2e93a5146f7436ed0252
Nicolas Ibañez <nicowxdvd@gmail.com>
2026-08-21 14:37:01 -0400

    inyectamos el token y la interfaz en el modulo de usuario

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,18 @@ import { UsersController } from './users.controller';
 import { AuthModule } from '../auth/auth.module';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { USER_REPOSITORY_TOKEN } from './interface/user-repository.interface';
 import { UserRepository } from './repositories/user.repository';
 
 @Module({
   imports: [AuthModule, TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, UserRepository],
+  providers: [
+    UsersService,
+    {
+      provide: USER_REPOSITORY_TOKEN,
+      useClass: UserRepository,
+    },
+  ],
 })
 export class UsersModule {}
