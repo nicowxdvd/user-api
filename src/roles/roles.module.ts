012ae442835ebc1486410b773ccca74a58012c7c
Nicolas Ibañez <nicowxdvd@gmail.com>
2026-08-21 13:20:58 -0400

    configuramos el modulo paa que el service  se asocie al token

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -4,11 +4,17 @@ import { RolesController } from './roles.controller';
 import { AuthModule } from '../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
+import { ROLE_REPOSITORY_TOKEN } from './interfaces/role-repository.interface';
 import { RoleRepository } from './repositories/role.repository';
-
 @Module({
   imports: [AuthModule, TypeOrmModule.forFeature([Role])],
   controllers: [RolesController],
-  providers: [RolesService, RoleRepository],
+  providers: [
+    RolesService,
+    {
+      provide: ROLE_REPOSITORY_TOKEN,
+      useClass: RoleRepository,
+    },
+  ],
 })
 export class RolesModule {}
