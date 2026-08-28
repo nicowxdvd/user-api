837cd09223a48470504503a79816b995b469b5fd
Nicolas Ibañez <nicowxdvd@gmail.com>
2026-08-28 16:53:45 -0400

    se crea el module

--- src/user-profiles/user-profiles.module.ts
+++ src/user-profiles/user-profiles.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserProfilesService } from './user-profiles.service';
 import { UserProfilesController } from './user-profiles.controller';
+import { AuthModule } from '../auth/auth.module';
+import { UserProfile } from './entities/user-profile.entity';
+import { USER_PROFILE_REPOSITORY_TOKEN } from './interfaces/user-profile-repository.interface';
+import { UserProfileRepository } from './repositories/user-profile.repository';
 
 @Module({
+  imports: [AuthModule, TypeOrmModule.forFeature([UserProfile])],
   controllers: [UserProfilesController],
-  providers: [UserProfilesService],
+  providers: [
+    UserProfilesService,
+    {
+      provide: USER_PROFILE_REPOSITORY_TOKEN,
+      useClass: UserProfileRepository,
+    },
+  ],
 })
 export class UserProfilesModule {}
