536c24e7ebd19dac8497113b60e393fa1003f199
Nicolas Ibañez <nicowxdvd@gmail.com>
2026-09-13 08:56:21 -0300

    style(main): formato compacto de code-style en bootstrap

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
   await app.listen(3001);
+
 }
 bootstrap();
