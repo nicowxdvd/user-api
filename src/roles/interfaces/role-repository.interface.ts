ac5c9daffbc80550683d60c124d421a872c15b65
Nicolas Ibañez <nicowxdvd@gmail.com>
2026-09-13 08:58:05 -0300

    style(roles): regla 15 de code-style en IRoleRepository

--- src/roles/interfaces/role-repository.interface.ts
+++ src/roles/interfaces/role-repository.interface.ts
@@ -5,12 +5,10 @@ import { Role } from '../entities/role.entity';
 export const ROLE_REPOSITORY_TOKEN = Symbol('ROLE_REPOSITORY_TOKEN');
 
 export interface IRoleRepository {
-  save(createRoleDto: CreateRoleDto): Promise<Role>;
-  findAll(isActive?: boolean): Promise<Role[]>;
-  findById(id: number): Promise<Role | null>;
-  updateStatus(
-    id: number,
-    isActive: boolean,
-  ): Promise<{ affected?: number | null }>;
-  delete(id: number): Promise<DeleteResult>;
+  save(createRoleDto: CreateRoleDto)         : Promise<Role>;
+  findAll(isActive?: boolean)                : Promise<Role[]>;
+  findById(id: number)                       : Promise<Role | null>;
+  updateStatus(id: number, isActive: boolean): Promise<{ affected?: number | null }>;
+  delete(id: number)                         : Promise<DeleteResult>;
+
 }
